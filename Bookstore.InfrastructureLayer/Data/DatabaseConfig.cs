2e5780339c054ea442792d131989f77eead0e355
Mandraszewsky <mandrah@o2.pl>
2025-01-02 20:23:05 +0100

    Minor refactor.

--- Bookstore.InfrastructureLayer/Data/DatabaseConfig.cs
+++ Bookstore.InfrastructureLayer/Data/DatabaseConfig.cs
@@ -2,5 +2,5 @@
 
 public static class DatabaseConfig
 {
-    public static string ConnectionString = @"Data Source=DESKTOP-I57J3OL;Initial Catalog=BookstoreDB;User Id=sa;Password=sa1234;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True;";
+    public static string ConnectionString = @"Data Source=DESKTOP-I111222;Initial Catalog=BookstoreDB;User Id=sa;Password=sa;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True;";
 }
